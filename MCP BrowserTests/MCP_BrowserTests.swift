2e615f0123916912df74628e172e72d18bf09646
Ben <brainfuel@icloud.com>
2026-04-23 18:52:21 +0700

    Initial Commit

---
+++ MCP BrowserTests/MCP_BrowserTests.swift
@@ -0,0 +1,16 @@
+//
+//  MCP_BrowserTests.swift
+//  MCP BrowserTests
+//
+//  Created by Ben Milford on 23/04/2026.
+//
+
+import Testing
+
+struct MCP_BrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
